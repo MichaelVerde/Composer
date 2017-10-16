80670399852ffb3d2b97ea4972f042b7e8580e6a
Michael Verde <michael.verde2@gmail.com>
2017-10-16 02:10:16 -0400

    Add JSON File uploading and error handling, add save deleting

--- src/app/saves/saves.component.ts
+++ src/app/saves/saves.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Save } from './save'
 import { SavesService } from './saves.service'
 
@@ -8,9 +8,11 @@ import { SavesService } from './saves.service'
   styleUrls: ['./saves.component.css']
 })
 export class SavesComponent implements OnInit {
+  @ViewChild('fileInput') fileInput;
   saveName: string = "";
   savesList: Save[];
   currentSave: number; 
+  uploadMsg: string = "";
 
   constructor(public savesService: SavesService) {}
 
@@ -30,6 +32,11 @@ export class SavesComponent implements OnInit {
     }
   }
 
+  deleteSave(idx: number){
+    this.savesService.saves.splice(idx,1);
+    this.savesService.saveChanged();
+  }
+
   selectSave(currentSave: number){
     this.currentSave = currentSave;
     this.savesService.selectSave(currentSave);
@@ -41,12 +48,48 @@ export class SavesComponent implements OnInit {
   }
 
   downloadJSON(idx: number){
-    var myjson = JSON.stringify(this.savesService.saves[idx]);
-    console.log(myjson);
-    var x = window.open();
-    x.document.open();
-    x.document.write('<html><body><pre>' + myjson + '</pre></body></html>');
-    x.document.close();
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.savesService.saves[idx]));
+    var dlAnchorElem = document.getElementById('downloadAnchorElem');
+    dlAnchorElem.setAttribute("href",     dataStr     );
+    dlAnchorElem.setAttribute("download", this.savesService.saves[idx].name + ".json");
+    dlAnchorElem.click();
+  }
+
+  fileUpload($event: any){
+    let file = $event.target.files[0]; 
+    let fileInput = this.fileInput.nativeElement;
+    var parent = this;
+    if(file){
+      let reader = new FileReader();
+      let savesService = this.savesService;
+
+      /* onLoad event is fired when the load completes. */
+      reader.onload = function(event: any) {
+          let content = event.target.result; 
+          try{
+            savesService.saves.push(Save.serialize(JSON.parse(content))); 
+            savesService.saveChanged();
+            setTimeout(()=> {
+              parent.uploadMsg = "";
+              fileInput.value = null;
+            }, 0);
+          }
+          catch(e){
+            setTimeout(()=> {
+              parent.uploadMsg = "Error Occured. Please upload a valid JSON file.";
+              fileInput.value = null;
+            }, 0);
+          }
+      };
+      /* The readAsText method will read the file's data as a text string. By default the string is decoded as 'UTF-8'. */
+      reader.readAsText(file);
+    }
+    else{
+      setTimeout(()=> {
+        parent.uploadMsg = "No File. Please upload a valid JSON file.";
+        fileInput.value = null;
+      }, 0);
+    }
   }
 
 }
